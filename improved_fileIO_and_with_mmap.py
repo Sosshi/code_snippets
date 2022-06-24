28c9f041324f78aeb04a4ff8b30d67f33e120ea5
oryon-dominik <symonmwenex@gmail.com>
2022-06-24 21:09:36 +0200

    test

--- improved_fileIO_and_with_mmap.py
+++ improved_fileIO_and_with_mmap.py
@@ -5,18 +5,14 @@ def regular_io(file):
         print(text)
 
 
-regular_io("a.txt")
 # Fast and emproved
 import mmap
 
 
-def mmap_io(filename):
-    with open(filename, mode="r", encoding="utf8") as file_obj:
+def mmap_io(file):
+    with open(file, mode="r", encoding="utf8") as file_obj:
         with mmap.mmap(
             file_obj.fileno(), length=0, access=mmap.ACCESS_READ
         ) as mmap_obj:
             text = mmap_obj.read()
-            print(text)
-
-
-mmap_io("a.txt")
+            print(text, 3)
